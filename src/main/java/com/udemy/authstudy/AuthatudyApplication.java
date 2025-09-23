f4fc78d650c60763cab50fc7f24d6802aa760036
Cristiane Santana <cristiane.souza1234@hotmail.com>
2025-09-22 23:13:51 -0300

    Commit inicial

---
+++ src/main/java/com/udemy/authstudy/AuthatudyApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.authstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthatudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthatudyApplication.class, args);
+	}
+
+}
